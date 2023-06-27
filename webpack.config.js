d3221516d825ac424c4185fad0af2bdb10c60893
Kidd254 <lorenkioko@gmail.com>
2023-06-27 15:30:50 +0300

    update webpack.config.js file to accomodate fonts

--- webpack.config.js
+++ webpack.config.js
@@ -2,41 +2,48 @@ const path = require('path');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
- module.exports = {
-   mode: 'development',
-   entry: {
-     index: './src/index.js',   
-   },
-   devServer: {
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+  },
+  devServer: {
     static: './dist',
   },
   plugins: [
 
     new HtmlWebpackPlugin({
-      template: "./src/index.html",
+      template: './src/index.html',
 
       title: 'JS Capstone Group Project',
 
     }),
 
   ],
-   output: {
-     filename: 'index.js',
-     path: path.resolve(__dirname, 'dist'),
-   },
-   module: {
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
     rules: [
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
-       {
+      {
 
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
 
         type: 'asset/resource',
 
       },
+      {
+
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+
+        type: 'asset/resource',
+
+      },
     ],
   },
- };
+};
