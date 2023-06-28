6578754d0ccfb658dde192807c55509fb8f97c5c
HFG43 <hernanguemes@hotmail.com>
2023-06-28 01:30:03 -0300

    Add conditionale Image Display
    Ensure all elements displayed have image != null from API input

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import './style.css';
-import Shows from './shows.js';
-import { showsContainer, searchForm, searchFormInput } from './dynamic.js';
+import "./style.css";
+import Shows from "./shows.js";
+import { showsContainer, searchForm, searchFormInput } from "./dynamic.js";
+import likes from './images/Empty_Like.svg';
 
 const setNewShow = (programList) => {
   let tvShows = [];
@@ -9,57 +10,68 @@ const setNewShow = (programList) => {
     const title = program.show.name;
     const like = false;
     const newShow = new Shows(id, image, title, like);
-    tvShows = [...tvShows, newShow];
+    if (image) {
+      tvShows = [...tvShows, newShow];
+    }
   });
   return tvShows;
 };
 
 const getTVShows = async (search) => {
   if (search === undefined) {
-    search = 'action';
+    search = "action";
   }
-  console.log(search);
   const resultPrograms = await fetch(
-    `https://api.tvmaze.com/search/shows?q=${search}`,
+    `https://api.tvmaze.com/search/shows?q=${search}`
   );
   const programList = await resultPrograms.json();
   const newProgramList = setNewShow(programList);
   return newProgramList;
 };
 
-const generateShowCard = async (search) => {
+const displayShows = async (search) => {
+  showsContainer.innerHTML = '';
   const tvShows = await getTVShows(search);
   tvShows.forEach((show) => {
-    const showCard = document.createElement('div');
-    showCard.classList.add('show_container');
-    const showImage = document.createElement('img');
-    showImage.classList.add('show_image');
+    const showCard = document.createElement("div");
+    showCard.classList.add("show_container");
+
+    const showImage = document.createElement("img");
+    showImage.classList.add("show_image");
     if (show.image) {
       showImage.src = show.image.original;
     } else {
-      showImage.src = '#';
+      showImage.src = "#";
     }
     showCard.appendChild(showImage);
-    const showTitle = document.createElement('h2');
-    showTitle.classList.add('card_title');
+
+    const titleLikeContainer = document.createElement('div');
+    titleLikeContainer.classList.add('title_like_container');
+
+    const showTitle = document.createElement("h2");
+    showTitle.classList.add("card_title");
     showTitle.innerText = show.title;
-    showCard.appendChild(showTitle);
-    // const likes = document.createElement('img');
-    // likes.src = '#';
-    // likes.alt = 'like_icon';
-    // showCard.appendChild(likes);
-    const commentButton = document.createElement('button');
-    commentButton.type = 'button';
-    commentButton.innerText = 'Comments';
-    commentButton.classList.add('comment_button');
+    titleLikeContainer.appendChild(showTitle);
+
+    const likesIcon = document.createElement('img');
+    likesIcon.src = likes;
+    likesIcon.alt = 'like_icon';
+    likesIcon.classList.add('like_icon');
+    titleLikeContainer.appendChild(likesIcon);
+    showCard.appendChild(titleLikeContainer);
+
+    const commentButton = document.createElement("button");
+    commentButton.type = "button";
+    commentButton.innerText = "Comments";
+    commentButton.classList.add("comment_button");
     showCard.appendChild(commentButton);
     showsContainer.appendChild(showCard);
   });
 };
 
-searchForm.addEventListener('submit', async (event) => {
+searchForm.addEventListener("submit", async (event) => {
   event.preventDefault();
   const search = searchFormInput.value;
   searchForm.reset();
-  generateShowCard(search);
+  displayShows(search);
 });
