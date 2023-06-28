d31dbca8e677d94414bc99defc5ac9cd0e96c04f
HFG43 <hernanguemes@hotmail.com>
2023-06-28 02:26:37 -0300

    Correct Linters - Style Input Validation

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import "./style.css";
-import Shows from "./shows.js";
-import { showsContainer, searchForm, searchFormInput } from "./dynamic.js";
+import './style.css';
+import Shows from './shows.js';
+import { showsContainer, searchForm, searchFormInput } from './dynamic.js';
 import likes from './images/Empty_Like.svg';
 
 const setNewShow = (programList) => {
@@ -19,43 +19,43 @@ const setNewShow = (programList) => {
 
 const getTVShows = async (search) => {
   if (search === undefined) {
-    search = "action";
+    search = 'action';
   }
   const resultPrograms = await fetch(
-    `https://api.tvmaze.com/search/shows?q=${search}`
+    `https://api.tvmaze.com/search/shows?q=${search}`,
   );
   const programList = await resultPrograms.json();
   const newProgramList = setNewShow(programList);
-  if(newProgramList.length <= 0) {
-    searchFormInput.placeholder = 'Please try with another TV Show';
+  if (newProgramList.length <= 0) {
+    searchFormInput.placeholder = 'Please try with another TV Show or Genre';
     searchFormInput.classList.add('error_input_show');
-       }
+  }
   return newProgramList;
 };
 
 const displayShows = async (search) => {
   showsContainer.innerHTML = '';
-  searchFormInput.placeholder = 'Type Search your favorite TV show or Genre';
+  searchFormInput.placeholder = 'Search for your favorite TV show or Genre';
   searchFormInput.classList.remove('error_input_show');
   const tvShows = await getTVShows(search);
   tvShows.forEach((show) => {
-    const showCard = document.createElement("div");
-    showCard.classList.add("show_container");
+    const showCard = document.createElement('div');
+    showCard.classList.add('show_container');
 
-    const showImage = document.createElement("img");
-    showImage.classList.add("show_image");
+    const showImage = document.createElement('img');
+    showImage.classList.add('show_image');
     if (show.image) {
       showImage.src = show.image.original;
     } else {
-      showImage.src = "#";
+      showImage.src = '#';
     }
     showCard.appendChild(showImage);
 
     const titleLikeContainer = document.createElement('div');
     titleLikeContainer.classList.add('title_like_container');
 
-    const showTitle = document.createElement("h2");
-    showTitle.classList.add("card_title");
+    const showTitle = document.createElement('h2');
+    showTitle.classList.add('card_title');
     showTitle.innerText = show.title;
     titleLikeContainer.appendChild(showTitle);
 
@@ -66,16 +66,16 @@ const displayShows = async (search) => {
     titleLikeContainer.appendChild(likesIcon);
     showCard.appendChild(titleLikeContainer);
 
-    const commentButton = document.createElement("button");
-    commentButton.type = "button";
-    commentButton.innerText = "Comments";
-    commentButton.classList.add("comment_button");
+    const commentButton = document.createElement('button');
+    commentButton.type = 'button';
+    commentButton.innerText = 'Comments';
+    commentButton.classList.add('comment_button');
     showCard.appendChild(commentButton);
     showsContainer.appendChild(showCard);
   });
 };
 
-searchForm.addEventListener("submit", async (event) => {
+searchForm.addEventListener('submit', async (event) => {
   event.preventDefault();
   const search = searchFormInput.value;
   searchForm.reset();
