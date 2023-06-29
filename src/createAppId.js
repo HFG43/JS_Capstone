da1acd6b4942a1ade3f207793d9cdd9e420017be
HFG43 <hernanguemes@hotmail.com>
2023-06-29 10:33:20 -0300

    Set Get function in API

--- src/createAppId.js
+++ src/createAppId.js
@@ -1,10 +1,28 @@
+// const postAppForID = async () => {
+//   const response = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/', {
+//     method: 'POST',
+//   });
+
+//   if (response.ok) {
+//     const data = await response.text;
+//     console.log(data);
+//     const nameResult = data.result;
+//     return nameResult;
+//   }
+//   return response;
+// };
 async function postAppForID() {
-  const response = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/', {
-    method: 'POST',
-  });
-
-  const result = await response.json();
-  console.log('Success:', result);
-};
-
+  const response = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/',
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  const appId = await response.text();
+  console.log(appId);
+  return appId;
+}
 export default postAppForID;
+
+// UgcwqV0d93hZ6oEAsKZi
