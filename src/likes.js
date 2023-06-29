e599c54585d9ea35f75a013ec9ce641ff765239d
HFG43 <hernanguemes@hotmail.com>
2023-06-29 12:37:39 -0300

    Add function to get Likes
    Get Array working ok!

--- src/likes.js
+++ src/likes.js
@@ -19,14 +19,20 @@ const postLike = async (showid) => {
   );
   const postLikesResponse = await response.text();
   console.log(postLikesResponse);
+  await getLikes();
   return postLikesResponse;
+};
+
+const getLikes = async () => {
+  const getAllLikes = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/Bi5YfurxCUYnE9wwCG8E/likes');
+  const data = await getAllLikes.json();
+  console.log(data);
+  return data;
 };
 
 const addLikes = async (search) => {
   const showid = search.id;
-  console.log(search);
-  console.log(showid);
-  await postLike(showid);
+  await getLikes();
   return showid;
 };
 
