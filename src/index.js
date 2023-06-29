b85218387bb52b658411f299fd27b3e9b1f73c4a
Kidd254 <lorenkioko@gmail.com>
2023-06-29 22:58:57 +0300

    modify the comment button to display comments

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import { showsContainer, searchForm, searchFormInput } from './dynamic.js';
 import likes from './images/Empty_Like.svg';
 import popUp from './popUp.js';
 import getTVShows from './getTvShow.js';
+import { getMovieData } from './comments.js';
 
 const displayShows = async (search) => {
   showsContainer.innerHTML = '';
   searchFormInput.placeholder = 'Search for your favorite TV show or Genre';
   searchFormInput.classList.remove('error_input_show');
   const tvShows = await getTVShows(search);
-  tvShows.forEach((show) => {
+  tvShows.forEach(async (show) => {
     const showCard = document.createElement('div');
     showCard.classList.add('show_container');
 
@@ -43,7 +44,31 @@ const displayShows = async (search) => {
     commentButton.id = show.id;
     commentButton.classList.add('comment_button');
     commentButton.addEventListener('click', async () => {
+      // Fetch the comments for the specific show
+      show.comments = await getMovieData(show.id);
+
+      // Call popUp function after setting the comments
       await popUp(show);
+
+      const updatedShow = await getMovieData(show.id);
+      show.comments = updatedShow;
+
+      const commentList = document.getElementById('comments-list');
+      commentList.innerHTML = '';
+
+      if (show.comments.length > 0) {
+        show.comments.forEach((comment) => {
+          const itemList = document.createElement('li');
+          itemList.innerHTML = `
+        <div>
+          <span>${comment.creation_date} </span>
+          <span class="user-name">${comment.username}: </span>
+          <span>${comment.comment}</span>
+        </div>
+      `;
+          commentList.append(itemList);
+        });
+      }
     });
     showCard.appendChild(commentButton);
     showsContainer.appendChild(showCard);
