7dab9787c876ab082798ffe43f1b7cd5c6eb779b
Kidd254 <lorenkioko@gmail.com>
2023-07-01 01:55:22 +0300

    add changes

---
+++ src/allItemsCounter.js
@@ -0,0 +1,13 @@
+import { selectShows, displayAllCounter } from './dynamic.js';
+
+const countAll = async () => {
+  const counter = selectShows.length;
+  return counter;
+};
+
+const displayCountAll = async () => {
+  const countValue = (await countAll()).toString();
+  displayAllCounter.innerText = countValue;
+};
+
+export default displayCountAll;
